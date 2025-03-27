483609db089c10e360b18314a710f450c7f8bcb6
SumitPShrestha <sumitpshrestha@gmail.com>
2025-03-27 22:10:57 +0545

    Add cmMicroService/cmSecurityService/src/main/java/com/cm/securityservice/ServletInitializer.java 1743092755628

---
+++ cmMicroService/cmSecurityService/src/main/java/com/cm/securityservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cm.securityservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SecurityServiceApplication.class);
+    }
+
+}
