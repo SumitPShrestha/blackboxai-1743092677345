5d4cf54c98d4a7bddfd720e3e349e6524a5a5263
SumitPShrestha <sumitpshrestha@gmail.com>
2025-03-27 22:12:27 +0545

    Add cmMicroService/cmConfigurationService/src/main/kotlin/com/cm/cmconfigurationservice/CmconfigurationsserviceApplication.kt 1743092846337

---
+++ cmMicroService/cmConfigurationService/src/main/kotlin/com/cm/cmconfigurationservice/CmconfigurationsserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.cm.cmconfigurationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CmconfigurationsserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CmconfigurationsserviceApplication>(*args)
+}
