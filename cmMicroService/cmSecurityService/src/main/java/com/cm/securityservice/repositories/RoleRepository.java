2b042e01fade6cb456f37015531a56331cedd1c7
SumitPShrestha <sumitpshrestha@gmail.com>
2025-03-27 22:10:28 +0545

    Add cmMicroService/cmSecurityService/src/main/java/com/cm/securityservice/repositories/RoleRepository.java 1743092726941

---
+++ cmMicroService/cmSecurityService/src/main/java/com/cm/securityservice/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.cm.securityservice.repositories;
+
+import com.cm.securityservice.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
